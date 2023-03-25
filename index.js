cd055527c722ba5348c21a7c5272b1e7a24705a8
taha-junaid <113802375+taha-junaid@users.noreply.github.com>
2023-03-25 14:35:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+    var submitButton = document.querySelector('.submit-button');
+
+    // Add an event listener to the submit button
+    submitButton.addEventListener('click', function () {
+        var file = document.getElementById('uploaded_file').files[0];
+        var customLabels = document.getElementById("custom_labels");
+        // console.log(custom_labels.value);
+        var reader = new FileReader();
+
+        reader.onload = function (event) {//
+            // var body = btoa(event.target.result.replace(/^data:(.*;base64,)?/, ''));
+            var body = event.target.result;
+            // console.log(body);
+            // console.log(file.type);
+            var filename = Date.now() + file.name
+            // console.log(filename)
+            var params = {
+                'bucket': 'pictures-assignment2',
+                'filename': filename,
+                'Content-Type': file.type,
+                'x-amz-meta-customLabels': customLabels.value,
+            };
+            sdk.bucketFilenamePut(params, body, {})
+                .then(function (res) {
+                    if (res.status == 200) {
+                        alert(filename, "IMAGE UPLOADED!!")
+                    }
+                });
+        }
+        reader.readAsDataURL(file);
+    }, false);
+
+    // ---------------------------------------------- audio search ------------------------------------------------------
+    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+    if ('SpeechRecognition' in window) {
+        console.log("SpeechRecognition is Working");
+    } else {
+        console.log("SpeechRecognition is Not Working");
+    }
+
+    var inputSearchQuery = document.getElementById("search_query");
+    const recognition = new window.SpeechRecognition();
+    //recognition.continuous = true;
+
+    micButton = document.getElementById("mic_search");
+
+    micButton.addEventListener('click', function () {
+        if (micButton.innerHTML == "mic") {
+            recognition.start();
+        } else if (micButton.innerHTML == "mic_off") {
+            recognition.stop();
+        }
+
+        recognition.addEventListener("start", function () {
+            micButton.innerHTML = "mic_off";
+            console.log("Recording.....");
+        });
+
+        recognition.addEventListener("end", function () {
+            console.log("Stopping recording.");
+            micButton.innerHTML = "mic";
+        });
+
+        recognition.addEventListener("result", resultOfSpeechRecognition);
+        function resultOfSpeechRecognition(event) {
+            const current = event.resultIndex;
+            transcript = event.results[current][0].transcript;
+            inputSearchQuery.value = transcript;
+            console.log("transcript : ", transcript)
+        }
+    });
+
+    // ---------------------------------------------- get search results ------------------------------------------------------
+
+    var searchPhotosButton = document.getElementById('searchPhotos');
+    var searchResultsDiv = document.getElementById('searchResults');
+
+    searchPhotosButton.addEventListener('click', function () {
+
+        async function fetchImages() {
+            var params = {
+                'q': inputSearchQuery.value
+            };
+            try {
+                const response = await sdk.searchGet(params, {}, {});
+                testArr = ["https://pictures-assignment2.s3.amazonaws.com/1679631501092cat4.jpeg", "https://pictures-assignment2.s3.amazonaws.com/1679631501092cat4.jpeg", "https://pictures-assignment2.s3.amazonaws.com/1679631501092cat4.jpeg"]
+                // for (const imageUrl of response['data']['data']) {
+                for (const imageUrl of testArr) {
+                    const imageResponse = await fetch(imageUrl);
+                    if (!imageResponse.ok) {
+                        throw new Error('Network response was not ok.');
+                    }
+                    const img = document.createElement('img');
+                    img.src = await imageResponse.text();
+                    searchResultsDiv.appendChild(img);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchImages();
+    });
+
+});
+
+ // try {
+            //     const response = await fetch('https://pictures-assignment2.s3.amazonaws.com/1679631501092cat4.jpeg');
+            //     if (!response.ok) {
+            //         throw new Error('Network response was not ok.');
+            //     }
+            //     const text = await response.text();
+            //     catImage.src = text;
+            // } catch (error) {
+            //     console.error('Error fetching image:', error);
+            // }
---
+++ lambda_function_1.py
@@ -0,0 +1,59 @@
+import logging
+import base64
+import json
+import boto3
+import os
+import requests
+import datetime
+from io import BytesIO
+
+def lambda_handler(event, context):
+    # print(event)
+    
+    Trigger_info = event['Records'][0]['s3']
+
+    BucketName = Trigger_info['bucket']['name']
+    KeyName = Trigger_info['object']['key']
+    # BucketName = 'pictures-assignment2'
+    # KeyName = '1679696031006cat4.jpeg'
+    # print("bucketname",BucketName )
+    # print("keyname",KeyName )
+    
+    s3 = boto3.client('s3')
+    s3obj = s3.get_object(Bucket=BucketName, Key=KeyName)
+
+    customlabels = s3obj['ResponseMetadata']['HTTPHeaders']['x-amz-meta-customlabels'].split(',')
+
+    base64_image_binary = s3obj['Body'].read() #this contains data:image/jpeg;base64, 
+    base64_image_string_only_data = base64_image_binary.decode('utf-8').split(',')[1]
+    image_bytes = bytes(base64.b64decode(base64_image_string_only_data))
+    
+    client = boto3.client('rekognition')
+    rekognitionImageObj = {'Bytes': image_bytes}
+    resp = client.detect_labels(Image=rekognitionImageObj)
+
+    timestamp =str(datetime.datetime.now())
+    
+    labels = customlabels
+
+    for i in range(len(resp['Labels'])):
+        labels.append(resp['Labels'][i]['Name'].lower())
+    
+    indexObj = {"objectKey": KeyName,"bucket":BucketName,"createdTimestamp":timestamp,"labels":labels}
+    
+    url = "https://search-pictures-t6qyfsrwuavky5fac4hs4wmrpq.us-east-1.es.amazonaws.com/picturess/0"
+    
+    #url = "https://search-photoboot-53tyon3nsbikh7e7qkyc26ivzm.us-west-2.es.amazonaws.com/photos/_doc"
+   
+    headers = {"Content-Type": "application/json"}
+    
+    #r = requests.post(url, data=json.dumps(indexObj).encode("utf-8"), headers=headers)
+    
+    #r = requests.post(url, data=json.dumps(indexObj).encode("utf-8"), headers=headers,auth=('Harish2023', 'Winwin2023$'))
+    
+    # print(r.text)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(indexObj)
+    }
